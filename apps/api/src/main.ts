010f30305831eebca35247bf517ff865a242cd64
Dr Dreo <drdreo@users.noreply.github.com>
2022-12-13 20:17:53 +0100

    Update main.ts

--- apps/api/src/main.ts
+++ apps/api/src/main.ts
@@ -4,9 +4,25 @@ import { NestFactory } from "@nestjs/core";
 import { AppModule } from "./app/app.module";
 import { environment } from "./environments/environment";
 
+const allowlist = [
+    'http://localhost:4200',
+    'https://tell-i.pages.dev',
+    'https://tell-i.drdreo.com'
+];
+
 async function bootstrap() {
-	const app = await NestFactory.create(AppModule);
-	app.enableCors({ origin: environment.clientUrl });
+	const app = await NestFactory.create(AppModule);  
+  app.enableCors({
+                  origin: (origin: string, callback: Function) => {
+                      console.log(origin);
+                      if (allowlist.indexOf(origin) !== -1) {
+                          callback(null, { origin: true });
+                      } else {
+                          callback(new Error('Not allowed by CORS'),  { origin: false });
+                      }
+                 },
+  });
+  
 	const port = process.env.PORT || 3333;
 	await app.listen(port, () => {
 		Logger.log("Listening at http://localhost:" + port, "main.ts");
